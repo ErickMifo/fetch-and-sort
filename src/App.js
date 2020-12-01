bbfd924c48c81ab43f809a82f1552e9154c62d5e
Erick Yudi Fukuda Mifo <erickyudi99@gmail.com>
2020-12-01 16:53:25 -0300

    ADD
    +fetch data from github
    +button to sort the data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
 
 function App() {
+
+  const[inputName, setInputName] = useState('')
+  const[name, setName] = useState('');
+  const[data, setData] = useState([''])
+  const[click, setClick] = useState(true)
+
+
+  const instance = axios.create ({
+    baseURL: `https://api.github.com/users/${name}/repos`
+});
+
+
+ useEffect(() => {
+if (name === '') {
+  return null
+} else {
+    async function getData() {
+        const request = await instance.get('')
+          setData(request.data)
+    }
+  
+    getData()
+ 
+
+  }}, [name]) 
+
+
   return (
+
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        setName(inputName)
+      }}>
+
+      <input onChange={(e) => {
+        setInputName(e.target.value)
+      }} type="text" autoComplete="off" />
+
+      <button> Pesquisar </button>
+      </form>
+        <button onClick={() => {
+          setClick(!click)
+          setData([...data.reverse()])}}> {click ? 'Ordem decrescente' : 'Ordem crescente'} </button>
+
+
+        <ul>
+{
+     data.map((item, index) => {
+       return (
+         <li key={index}> {item.name} </li>
+       )
+       })
+}
+       </ul>
+
+
     </div>
   );
 }
